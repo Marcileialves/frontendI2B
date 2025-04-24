8428e6bd01ab18d4f57380cacf080ea6a031b71e
Marcileialves <a.marcilei@estudante.ifro.edu.br>
2025-04-23 20:17:26 -0400

    first commit

---
+++ java.js
@@ -0,0 +1,24 @@
+const nome = "Marcilei";
+const idade = 16;
+console.log(`olá meu nome é ${nome} e tenho ${idade} anos`);
+const test = "string";
+console.log(test.toUpperCase());
+console.log(test.toLowerCase())
+
+console.log(`O tipo desta variável é ${typeof test}`)
+const num = 20;
+console.log(`O tipo desta variável é ${typeof num}`)
+
+const altura = 1.80;
+const peso = 80;
+const imc = peso / (altura * altura);
+console.log(`Seu IMC é ${imc.toFixed(2)}`)
+
+var n = 5;
+n++;
+n--;
+console.log(n)
+console.log(++n)
+console.log(--n)
+
+
